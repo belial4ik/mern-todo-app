845f3482799281aa4b8ff8e0282cf7b503d627d1
Belial <belyal4ik@gmail.com>
2022-02-04 19:18:27 +0300

    add routes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const tasks = require("./routes/tasks")
 const connection = require("./db")
 const cors = require("cors")
 const express = require('express')
@@ -5,15 +6,16 @@ const app = express()
 
 connection()
 
-const port = process.env.PORT || 8080
-
 app.use(express.json())
 app.use(cors())
 
+app.use("/api/tasks", tasks)
 
-
-
-
-
-
+const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}........`))
+
+
+
+
+
+Видео 8.07
